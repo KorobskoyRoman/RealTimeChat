32243bc997d2ce9daa9eb87824d537cd318b8c1f
KorobskoyRoman <shopone023@gmail.com>
2022-01-24 18:25:10 +0300

    Initial Commit

---
+++ RealTimeChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RealTimeChat
+//
+//  Created by Roman Korobskoy on 24.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
